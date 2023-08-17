cbbad141f61a627efe4065620e962efad20a7544
Verouge <branola1998628@gmail.com>
2023-08-17 22:48:48 +1000

    Update view employees by department

--- db/queries.js
+++ db/queries.js
@@ -69,6 +69,18 @@ class DBQueries {
       [managerId]
     );
   }
+
+  viewEmployeesByDepartment(departmentId) {
+    return db.query(
+      `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager 
+         FROM employee e 
+         JOIN role ON e.role_id = role.id 
+         JOIN department ON role.department_id = department.id 
+         LEFT JOIN employee m ON e.manager_id = m.id
+         WHERE department.id = ?`,
+      [departmentId]
+    );
+  }
 }
 
 module.exports = new DBQueries();
--- menus/viewEmployeesByDepartment.js
+++ menus/viewEmployeesByDepartment.js
@@ -1,7 +1,14 @@
 const queries = require("../db/queries");
+const promptForDepartment = require("../utils/promptForDepartment");
 
 async function viewEmployeesByDepartment() {
-  // Your logic here
+  const departmentId = await promptForDepartment(
+    "Which department's employees would you like to view?"
+  );
+  const employeesByDepartment = await queries.viewEmployeesByDepartment(
+    departmentId
+  );
+  console.table(employeesByDepartment[0]);
 }
 
 module.exports = viewEmployeesByDepartment;
