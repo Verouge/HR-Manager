4d964258036d982979af734e638b9ad16ab7162c
Verouge <branola1998628@gmail.com>
2023-08-17 19:14:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -22,14 +22,75 @@ function mainMenu() {
     ])
     .then((answer) => {
       switch (answer.action) {
-        case "View all departments":
-          // Call queries.viewAllDepartments() and then display the results
-          // Return to the main menu or handle other actions...
+        case "View all employees":
+          queries.viewAllEmployees().then((employees) => {
+            console.table(employees);
+            mainMenu();
+          });
           break;
 
-        // Handle other cases...
+        case "Add employee":
+          inquirer
+            .prompt([
+              { name: "first_name", message: "Enter employee's first name:" },
+              { name: "last_name", message: "Enter employee's last name:" },
+              //... other employee details
+            ])
+            .then((employeeDetails) => {
+              queries.addEmployee(employeeDetails).then(() => {
+                console.log("Employee added!");
+                mainMenu();
+              });
+            });
+          break;
+
+        case "Update employee role":
+          // Here, you'd get the list of employees, let the user pick one, then provide the roles to choose from, and then update.
+          // Simplified for brevity.
+          break;
+
+        case "View all roles":
+          queries.viewAllRoles().then((roles) => {
+            console.table(roles);
+            mainMenu();
+          });
+          break;
+
+        case "Add role":
+          inquirer
+            .prompt([
+              { name: "title", message: "Enter role title:" },
+              { name: "salary", message: "Enter role salary:" },
+              //... other role details
+            ])
+            .then((roleDetails) => {
+              queries.addRole(roleDetails).then(() => {
+                console.log("Role added!");
+                mainMenu();
+              });
+            });
+          break;
+
+        case "View all departments":
+          queries.viewAllDepartments().then((departments) => {
+            console.table(departments);
+            mainMenu();
+          });
+          break;
+
+        case "Add department":
+          inquirer
+            .prompt([{ name: "name", message: "Enter department name:" }])
+            .then((departmentDetails) => {
+              queries.addDepartment(departmentDetails).then(() => {
+                console.log("Department added!");
+                mainMenu();
+              });
+            });
+          break;
 
         case "Exit":
+          console.log("Goodbye!");
           process.exit();
           break;
       }
