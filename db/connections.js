58ef7019ea49bd314b699c44cce3c0beb56f62c4
Verouge <branola1998628@gmail.com>
2023-08-17 20:08:11 +1000

    update files

--- db/connections.js
+++ db/connections.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "your_mysql_password",
-  database: "your_database_name",
+  password: "root",
+  database: "HR_db",
 });
 
 const db = connection.promise();
--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,4 @@
-const db = require("./connection");
+const db = require("./connections");
 
 class DBQueries {
   // View all departments
